a471b3147727ea3bdb4f78b52d7adde692157ae8
l19200895 <155929147+l19200895@users.noreply.github.com>
2024-01-29 10:23:25 -0600

    Update main.js

--- jason/main.js
+++ jason/main.js
@@ -1,60 +1,75 @@
 let ip = document.getElementById("ip");
-let paisInput = document.getElementById("pais");
+let pais = document.getElementById("pais");
 let continente = document.getElementById("continente");
 let ciudad = document.getElementById("ciudad");
 let zona_horaria = document.getElementById("zona_horaria");
 
 const solicitudAPI = () => {
-  // Hacer una petición según el país seleccionado
-  const pais = paisInput.value.toLowerCase(); // Convertir a minúsculas para hacer la comparación insensible a mayúsculas/minúsculas
+  // Hacer una petición para un usuario con ID especifico
+  axios.get("https://https://bddcentral.000webhostapp.com/location.examen/index.php")
+    .then(function (response) {
+      // manejar respuesta exitosa
+      console.log(response.data);
+      ip.innerHTML = response.data.ip;
+      pais.innerHTML = response.data.pais;
+      continente.innerHTML = response.data.continente;
+      ciudad.innerHTML = response.data.ciudad;
+      zona_horaria.innerHTML = response.data.zona_horaria;
 
-  if (pais === "mexico") {
-    axios.get("https://progwebform.000webhostapp.com/location-examen/index.php")
-      .then(function (response) {
-        // Manejar respuesta exitosa
-        console.log(response.data);
-        ip.innerHTML = response.data.ip;
-        paisInput.innerHTML = response.data.pais; // Modificar la variable de entrada
-        continente.innerHTML = response.data.continente;
-        ciudad.innerHTML = response.data.ciudad;
-        zona_horaria.innerHTML = response.data.zona_horaria;
-      })
-      .catch(function (error) {
-        // Manejar error
-        console.log(error);
-      });
-  } else if (pais === "canada") {
-    axios.get("https://bddcuenta2.000webhostapp.com/location_examen/index.php")
-      .then(function (response) {
-        console.log(response.data);
-        ip.innerHTML = response.data.ip;
-        paisInput.innerHTML = response.data.pais; // Modificar la variable de entrada
-        continente.innerHTML = response.data.continente;
-        ciudad.innerHTML = response.data.ciudad;
-        zona_horaria.innerHTML = response.data.zona_horaria;
-      })
-      .catch(function (error) {
-        // Manejar error
-        console.log(error);
-      });
-  } else if (pais === "south korea") {
-    axios.get("https://bddcuenta3.000webhostapp.com/location_examen/index.php")
-      .then(function (response) {
-        console.log(response.data);
-        ip.innerHTML = response.data.ip;
-        paisInput.innerHTML = response.data.pais; // Modificar la variable de entrada
-        continente.innerHTML = response.data.continente;
-        ciudad.innerHTML = response.data.ciudad;
-        zona_horaria.innerHTML = response.data.zona_horaria;
-      })
-      .catch(function (error) {
-        // Manejar error
-        console.log(error);
-      })
-      .finally(function () {
-        // Siempre será ejecutado
-      });
-  }
+      // Verifica si el continente es África
+      if (response.data.continente === 'Africa') {
+        // Hacer algo con la primera URL
+        axios.get("https://progwebform.000webhostapp.com/location-examen/index.php")
+          .then(primeraResponse => {
+            ip.innerHTML = primeraResponse.data.ip;
+            pais.innerHTML = primeraResponse.data.pais;
+            ciudad.innerHTML = primeraResponse.data.ciudad;
+            zona_horaria.innerHTML = primeraResponse.data.zona_horaria;
+            console.log(primeraResponse.data);
+          })
+          .catch(error => {
+            console.error('Error en la primera URL:', error);
+          });
+      } else if (response.data.continente === 'North America'){
+        // Hacer algo con la segunda URL
+        axios.get("https://bddcuenta2.000webhostapp.com/location_examen/index.php")
+          .then(segundaResponse => {
+            // Hacer algo con la respuesta de la segunda URL
+            console.log(segundaResponse.data);
+            ip.innerHTML = response.data.ip;
+            pais.innerHTML = response.data.pais;
+            ciudad.innerHTML = response.data.ciudad;
+            zona_horaria.innerHTML = response.data.zona_horaria;
+            console.log(primeraResponse.data);
+          })
+          .catch(error => {
+            console.error('Error en la segunda URL:', error);
+          });
+      } else if (response.data.continente === 'North America'){
+        // Hacer algo con la segunda URL
+        axios.get("https://bddcuenta2.000webhostapp.com/location_examen/index.php")
+          .then(terceraResponse => {
+            // Hacer algo con la respuesta de la segunda URL
+            console.log(terceraResponse.data);
+            ip.innerHTML = response.data.ip;
+            pais.innerHTML = response.data.pais;
+            ciudad.innerHTML = response.data.ciudad;
+            zona_horaria.innerHTML = response.data.zona_horaria;
+            console.log(terceraResponse.data);
+          })
+          .catch(error => {
+            console.error('Error en la segunda URL:', error);
+          });
+    })
+    .catch(function (error) {
+      // manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // siempre será ejecutado
+    });
 };
 
+// Llama al evento load cada vez que se actualiza la página y llama a la función solicitudAPI que tiene la rutina de 
+// llamar a la API desde axios
 window.onload = solicitudAPI;
