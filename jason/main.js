1692049e8d95df52a5b58daf25e430615d5a17ef
l19200895 <155929147+l19200895@users.noreply.github.com>
2024-01-29 01:55:30 -0600

    Update main.js

--- jason/main.js
+++ jason/main.js
@@ -1,4 +1,3 @@
-// prueba
 let ip=document.getElementById("ip");
 let pais=document.getElementById("pais");
 let continente=document.getElementById("continente");
@@ -7,25 +6,61 @@ let zona_horaria=document.getElementById("zona_horaria");
 
 const solicitudAPI = () => {
  // Hacer una petición para un usuario con ID especifico
-axios.get("https://progwebform.000webhostapp.com/location-examen/index.php")
-  .then(function (response) {
-    // manejar respuesta exitosa
-    console.log(response.data);
-    ip.innerHTML = response.data.ip;
-    pais.innerHTML = response.data.pais;
-    continente.innerHTML = response.data.continente;
-    ciudad.innerHTML = response.data.ciudad;
-    zona_horaria.innerHTML = response.data.zona_horaria;
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  }); 
-};
 
-// Llama al evento load cada vez que se actualiza la pagina y llama a la funcion solicitudAPI que tiene la rutina de 
-// llamar a la API desde axios
-window.onload = solicitudAPI;
+  if (pais === "Mexico") {
+  // Hacer la petición a la URL determinada
+  axios.get("https://progwebform.000webhostapp.com/location-examen/index.php")
+    .then(function (response) {
+      // Manejar respuesta exitosa
+      console.log(response.data);
+      ip.innerHTML = response.data.ip;
+      pais.innerHTML = response.data.pais;
+      continente.innerHTML = response.data.continente;
+      ciudad.innerHTML = response.data.ciudad;
+      zona_horaria.innerHTML = response.data.zona_horaria;
+    })
+    .catch(function (error) {
+      // Manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // Siempre será ejecutado
+    }); 
+     }else if(pais === "Canada") {
+      axios.get("https://bddcuenta2.000webhostapp.com/location_examen/index.php")
+    .then(function (response) {
+      console.log(response.data);
+      ip.innerHTML = response.data.ip;
+      pais.innerHTML = response.data.pais;
+      continente.innerHTML = response.data.continente;
+      ciudad.innerHTML = response.data.ciudad;
+      zona_horaria.innerHTML = response.data.zona_horaria;
+    })
+    .catch(function (error) {
+      // Manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // Siempre será ejecutado
+    }); 
+    }else if(pais === "korea") {
+      axios.get("https://bddcuenta3.000webhostapp.com/location_examen/index.php")
+     .then(function (response) {
+      console.log(response.data);
+      ip.innerHTML = response.data.ip;
+      pais.innerHTML = response.data.pais;
+      continente.innerHTML = response.data.continente;
+      ciudad.innerHTML = response.data.ciudad;
+      zona_horaria.innerHTML = response.data.zona_horaria;
+    })
+    .catch(function (error) {
+      // Manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // Siempre será ejecutado
+    }); 
+  }
+
+};
+    window.onload = solicitudAPI;
