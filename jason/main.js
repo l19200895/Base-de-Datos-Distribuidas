4537edbbae84ca9cd2cee3925c97442224e00a85
l19200895 <155929147+l19200895@users.noreply.github.com>
2024-01-29 01:59:11 -0600

    Update main.js

--- jason/main.js
+++ jason/main.js
@@ -6,7 +6,24 @@ let zona_horaria=document.getElementById("zona_horaria");
 
 const solicitudAPI = () => {
  // Hacer una petición para un usuario con ID especifico
-
+axios.get("https://progwebform.000webhostapp.com/php-geoip-api/index.php")
+  .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response.data);
+    ip.innerHTML = response.data.ip;
+    pais.innerHTML = response.data.pais;
+    continente.innerHTML = response.data.continente;
+    ciudad.innerHTML = response.data.ciudad;
+    zona_horaria.innerHTML = response.data.zona_horaria;
+  })
+  .catch(function (error) {
+    // manejar error
+    console.log(error);
+  })
+  .finally(function () {
+    // siempre sera executado
+  }); 
+};
   if (pais === "Mexico") {
   // Hacer la petición a la URL determinada
   axios.get("https://progwebform.000webhostapp.com/location-examen/index.php")
