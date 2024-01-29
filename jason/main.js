54f9f7e9afc8286fd2bb5c6296e2c32331645b53
l19200895 <155929147+l19200895@users.noreply.github.com>
2024-01-28 21:19:02 -0600

    Add files via upload

--- jason/main.js
+++ jason/main.js
@@ -1,3 +1,4 @@
+
 let ip=document.getElementById("ip");
 let pais=document.getElementById("pais");
 let continente=document.getElementById("continente");
@@ -5,26 +6,38 @@ let ciudad=document.getElementById("ciudad");
 let zona_horaria=document.getElementById("zona_horaria");
 
 const solicitudAPI = () => {
- // Hacer una petición para un usuario con ID especifico
-axios.get("https://progwebform.000webhostapp.com/location-examen/index.php")
-  .then(function (response) {
-    // manejar respuesta exitosa
-    console.log(response.data);
-    ip.innerHTML = response.data.ip;
-    pais.innerHTML = response.data.pais;
-    continente.innerHTML = response.data.continente;
-    ciudad.innerHTML = response.data.ciudad;
-    zona_horaria.innerHTML = response.data.zona_horaria;
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  }); 
+  let apiUrl;
+
+  // Verificar el valor de la variable "pais" y asignar la URL correspondiente
+  if (pais.innerHTML === "Mexico") {
+    apiUrl = "https://progwebform.000webhostapp.com/location-examen/index.php";
+  } else if (pais.innerHTML === "Korea") {
+    apiUrl = "https://bddcuenta2.000webhostapp.com/location_examen/index.php";
+  } else if (pais.innerHTML === "Canada") {
+    apiUrl = "https://bddcuenta3.000webhostapp.com/location_examen/index.php";
+  } else {
+    console.log("País no reconocido");
+    return; // Salir de la función si el país no es reconocido
+  }
+
+  // Hacer la petición a la URL determinada
+  axios.get(apiUrl)
+    .then(function (response) {
+      // Manejar respuesta exitosa
+      console.log(response.data);
+      ip.innerHTML = response.data.ip;
+      pais.innerHTML = response.data.pais;
+      continente.innerHTML = response.data.continente;
+      ciudad.innerHTML = response.data.ciudad;
+      zona_horaria.innerHTML = response.data.zona_horaria;
+    })
+    .catch(function (error) {
+      // Manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // Siempre será ejecutado
+    });
 };
 
-// Llama al evento load cada vez que se actualiza la pagina y llama a la funcion
-// solicitudAPI que tiene la rutina de llamar a la API desde axios
 window.onload = solicitudAPI;
