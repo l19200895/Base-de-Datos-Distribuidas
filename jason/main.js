3f3026a282e46a11d8bace10c59603940ce6b03f
l19200895 <155929147+l19200895@users.noreply.github.com>
2024-01-29 01:16:11 -0600

    Update main.js

--- jason/main.js
+++ jason/main.js
@@ -1,87 +1,51 @@
-let ip = document.getElementById("ip");
-let pais = document.getElementById("pais").value;
-let continente = document.getElementById("continente");
-let ciudad = document.getElementById("ciudad");
-let zona_horaria = document.getElementById("zona_horaria");
+let ip=document.getElementById("ip");
+let pais=document.getElementById("pais");
+let continente=document.getElementById("continente");
+let ciudad=document.getElementById("ciudad");
+let zona_horaria=document.getElementById("zona_horaria");
 
 const solicitudAPI = () => {
-
-  // Hacer la petición a la URL determinada según el valor de "pais"
-  if (pais === "Mexico") {
-    // No hacer la petición a la base de datos, solo mostrar los resultados
-    console.log("Mostrar resultados para México");
-    mostrarResultados();
-
-  } else if (pais === "Korea") {
-    // Hacer la petición a la URL determinada
-    axios.get("https://progwebform.000webhostapp.com/location-examen/index.php")
-      .then(function (response) {
-        // Manejar respuesta exitosa
-        console.log(response.data);
-        guardarResultados(response.data);
-        mostrarResultados();
-      })
-      .catch(function (error) {
-        // Manejar error
-        console.log(error);
-      })
-      .finally(function () {
-        // Siempre será ejecutado
-      });
-
-  } else if (pais === "Canada") {
-    // Hacer la petición a la URL determinada
-    axios.get("https://bddcuenta2.000webhostapp.com/location_examen/index.php")
-      .then(function (response) {
-        // Manejar respuesta exitosa
-        console.log(response.data);
-        guardarResultados(response.data);
-        mostrarResultados();
-      })
-      .catch(function (error) {
-        // Manejar error
-        console.log(error);
-      })
-      .finally(function () {
-        // Siempre será ejecutado
-      });
-
-  } else if (pais === "Ucrania") {
-    // Hacer la petición a la URL determinada
-    axios.get("https://bddcuenta3.000webhostapp.com/location_examen/index.php")
-      .then(function (response) {
-        // Manejar respuesta exitosa
-        console.log(response.data);
-        guardarResultados(response.data);
-        mostrarResultados();
-      })
-      .catch(function (error) {
-        // Manejar error
-        console.log(error);
-      })
-      .finally(function () {
-        // Siempre será ejecutado
-      });
-
-  } else {
-    console.log("País no reconocido");
-    mostrarResultados();
-  }
+ // Hacer una petición para un usuario con ID especifico
+axios.get("https://progwebform.000webhostapp.com/php-geoip-api/index.php")
+  .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response.data);
+    ip.innerHTML = response.data.ip;
+    pais.innerHTML = response.data.pais;
+    continente.innerHTML = response.data.continente;
+    ciudad.innerHTML = response.data.ciudad;
+    zona_horaria.innerHTML = response.data.zona_horaria;
+  })
+  .catch(function (error) {
+    // manejar error
+    console.log(error);
+  })
+  .finally(function () {
+    // siempre sera executado
+  }); 
 };
-
-const guardarResultados = (data) => {
-  // Guardar los resultados en la base de datos o tomar otras acciones necesarias
-  console.log("Guardar resultados en la base de datos:", data);
-  // Aquí puedes agregar la lógica para guardar los resultados en la base de datos
-};
-
-const mostrarResultados = () => {
-  // Mostrar los resultados en la interfaz de usuario
-  ip.innerHTML = "Resultado IP";
-  continente.innerHTML = "Resultado Continente";
-  ciudad.innerHTML = "Resultado Ciudad";
-  zona_horaria.innerHTML = "Resultado Zona Horaria";
-};
-
-window.onload = solicitudAPI;
+if (pais === "Mexico") {
+  // Hacer la petición a la URL determinada
+  axios.get("https://progwebform.000webhostapp.com/location-examen/conexion.php")
+    .then(function (response) {
+      // Manejar respuesta exitosa
+      console.log(response.data);
+      ip.innerHTML = response.data.ip;
+      pais.innerHTML = response.data.pais;
+      continente.innerHTML = response.data.continente;
+      ciudad.innerHTML = response.data.ciudad;
+      zona_horaria.innerHTML = response.data.zona_horaria;
+    })
+    .catch(function (error) {
+      // Manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // Siempre será ejecutado
+    }); 
+     }else {
+      console.log("País no reconocido");
+      return; // Salir de la función si el país no es reconocido
+    };
+    window.onload = solicitudAPI;
 
